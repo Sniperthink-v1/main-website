e4a1c146fd1717dd0ecc9f8c6840a4bb2ddc8d2f
Aviral-Dwivedi-0 <aviraldwivedi.ad1@gmail.com>
2025-11-25 18:11:39 +0530

    Add Microsoft Clarity analytics script

--- app/layout.tsx
+++ app/layout.tsx
@@ -61,6 +61,16 @@ export default function RootLayout({
             gtag('config', 'G-73TLB4ZJQR');
           `}
         </Script>
+        {/* Microsoft Clarity */}
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "ubmch1gsef");
+          `}
+        </Script>
         <Script
           id="organization-schema"
           type="application/ld+json"
